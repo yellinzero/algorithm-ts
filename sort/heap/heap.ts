4dc04f81a17067c8c5ec9a28472e481da7c9e6df
ylinH-git <king22224444@163.com>
2022-01-03 21:47:22 +0800

    更正： 基于堆的优先队列实现

--- sort/heap/heap.ts
+++ sort/heap/heap.ts
@@ -3,7 +3,7 @@ import { MaxPQ, getType, isComparable, Comparable } from "../../common";
  *核心：利用堆优先队列实现排序
  *优先队列：支持 删除最大元素 和 插入元素 两种操作的抽象数据类型
  *我们可以通过数组、链表、堆等数据结构实现优先队列
- *以下使用堆 
+ *以下为基于堆的优先队列
  */
 export class Heap<T> extends MaxPQ<T> {
     private pq: T[];
