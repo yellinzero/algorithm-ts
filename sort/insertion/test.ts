95a634889cd600eaf5210239feae1ff54279c6cb
ylinH-git <69850810+ylinH-git@users.noreply.github.com>
2022-03-30 23:29:11 +0800

    Delete sort/insertion directory

--- sort/insertion/test.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ts-node
-import { Insertion } from './Insertion'
-const argvs = process.argv.splice(2)
-// examples
-const arr = [1, 2, 5, 6, 7, 7, 8, 123, 5, 2]
-Insertion.sort(arr)
-Insertion.show(arr)
