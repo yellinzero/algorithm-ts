36e33568d7bb661e673a2240be70abeb9c07ff82
ylinH-git <king22224444@163.com>
2022-05-30 22:27:09 +0800

    feat: 计算强连通分量的KosarajuSCC算法

---
+++ Graph/DFS/KosarajuSCC.ts
@@ -0,0 +1,43 @@
+/**
+ * 有向图的强连通分量
+ */
+import { Digraph } from "../common/Digraph"
+import { DepthFirstOrder } from "./DepthFirstOrder"
+
+export class KosarajuSCC {
+    private marked: boolean[]
+    private id: number[]
+    private count: number = 0
+
+    constructor(G: Digraph) {
+        this.marked = []
+        this.id = []
+        const order = new DepthFirstOrder(G.reverse())
+        for (let s of order.getReversePost()) {
+            if (!this.marked[s]) {
+                this.dfs(G, s)
+                this.count++
+            }
+        }
+    }
+
+    private dfs(G: Digraph, v: number) {
+        this.marked[v] = true
+        this.id[v] = this.count
+        for (let w of G.getAdj(v)) {
+            if (!this.marked[w]) this.dfs(G, w)
+        }
+    }
+
+    stronglyConnected(v: number, w: number) {
+        return this.id[v] === this.id[w]
+    }
+
+    getId(v: number) {
+        return this.id[v]
+    }
+
+    getCount() {
+        return this.count;
+    }
+}
---
+++ Graph/TestKosarajuSCC.ts
@@ -0,0 +1,33 @@
+#!/usr/bin/env ts-node
+
+import { Digraph } from "./common/Digraph";
+import { KosarajuSCC } from "./DFS/KosarajuSCC";
+import { stdout } from "process";
+
+const argvs = process.argv.splice(2)
+const fs = require('fs')
+let graphInfo = fs.readFileSync(argvs[0], { encoding: 'UTF-8' })
+
+const G = new Digraph(graphInfo)
+console.log(G)
+const cc = new KosarajuSCC(G)
+
+const M: number = cc.getCount()
+console.log(M + ' components')
+
+const components: number[][] = []
+for (let i = 0; i < M; i++) {
+    components[i] = []
+}
+for (let v = 0; v < G.getV(); v++) {
+    components[cc.getId(v)].unshift(v)
+}
+for (let i = 0; i < M; i++) {
+    let str = ''
+    for (let v of components[i]) {
+        str += v + ' '
+    }
+    console.log(str)
+}
+
+
