67ea060465e966a81abaa0bd178292f7283b983f
ylinH-git <king22224444@163.com>
2022-06-06 00:26:13 +0800

    feat: 低位优先的字符串排序

---
+++ String/LSD.ts
@@ -0,0 +1,26 @@
+export class LSD {
+    static sort(a: string[], W: number) {
+        const N = a.length
+        const R = 256
+        const aux = [] as string[]
+
+        for (let d = W - 1; d >= 0; d--) {
+            const count = new Array(R + 1)
+            count.fill(0)
+            for (let i = 0; i < N; i++) {
+                count[a[i].charCodeAt(d) + 1]++
+            }
+
+            for (let r = 0; r < R; r++) {
+                count[r + 1] += count[r]
+            }
+
+            for (let i = 0; i < N; i++) {
+                aux[count[a[i].charCodeAt(d)]++] = a[i]
+            }
+            for (let i = 0; i < N; i++) {
+                a[i] = aux[i]
+            }
+        }
+    }
+}
---
+++ String/TestLSD.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env ts-node
+
+import { LSD } from "./LSD"
+
+const a = ['4PGC938', '2IYE230', '3CI0720', '1ICK750', '1OHV845', '4JZY524', '1ICK750', '3CI0720', '1OHV845', '1OHV845', '2RLA629', '2RLA629', '3ATW723']
+
+LSD.sort(a, 7)
+
+console.log(a)
