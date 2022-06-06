3b55b3b8161684adb5bbe08601a5875255898dbe
ylinH-git <king22224444@163.com>
2022-06-06 08:33:11 +0800

    feat: MSD及Insertion扩展

---
+++ String/MSD.ts
@@ -0,0 +1,44 @@
+import { Insertion } from "../sort"
+
+export class MSD {
+    private static R = 256
+    private static M = 15
+    private static aux: string[]
+
+    private static charAt(s: string, d: number) {
+        if (d < s.length)
+            return s.charCodeAt(d)
+        else
+            return -1
+    }
+
+    static sort(a: string[], lo?: number, hi?: number, d?: number) {
+        if (lo !== undefined && hi !== undefined && d !== undefined) {
+            if (hi <= lo + this.M) {
+                Insertion.sort(a, lo, hi, d)
+                return
+            }
+            const count = new Array(this.R + 2)
+            count.fill(0)
+            for (let i = lo; i <= hi; i++) {
+                count[this.charAt(a[i], d) + 2]++
+            }
+            for (let r = 0; r < this.R + 1; r++) {
+                count[r + 1] += count[r]
+            }
+            for (let i = lo; i <= hi; i++) {
+                this.aux[count[this.charAt(a[i], d) + 1]++] = a[i]
+            }
+            for (let i = lo; i <= hi; i++) {
+                a[i] = this.aux[i - lo]
+            }
+            for (let r = 0; r < this.R; r++) {
+                this.sort(a, lo + count[r], lo + count[r + 1] - 1, d + 1)
+            }
+        } else {
+            const N = a.length
+            this.aux = []
+            this.sort(a, 0, N - 1, 0)
+        }
+    }
+}
--- sort/Insertion/Insertion.ts
+++ sort/Insertion/Insertion.ts
@@ -2,12 +2,25 @@ import { Sort } from "../Sort"
 
 // 核心：将当前值按大小插入到数组对应位置
 export class Insertion extends Sort {
-    static sort(a:any[]): void {
-        let N: number = a.length
-        for (let i = 1; i < N; i++) {
-            for (let j = i ; j > 0 && this.less(a[j], a[j - 1]); j--) {
-                this.exch(a, j, j - 1)
+    static sort(a: any[], lo?: number, hi?: number, d?: number): void {
+        if (lo !== undefined && hi !== undefined && d !== undefined) {
+            const less = (v: string, w: string, d: number) => {
+                return v.substring(d) < w.substring(d)
+            }
+            for (let i = lo; i <= hi; i++) {
+                for (let j = i; j > lo && less(a[j], a[j - 1], d); j--) {
+                    this.exch(a, j, j - 1)
+                }
             }
         }
+        else {
+            let N: number = a.length
+            for (let i = 1; i < N; i++) {
+                for (let j = i; j > 0 && this.less(a[j], a[j - 1]); j--) {
+                    this.exch(a, j, j - 1)
+                }
+            }
+        }
+
     }
 }
